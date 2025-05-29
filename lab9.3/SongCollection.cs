0a41042662e26388dd51fa43918cd6878efef560
angela8975 <angela.labushak@gmail.com>
2025-05-29 21:42:57 +0300

    Update SongCollection.cs

--- lab9.3/SongCollection.cs
+++ lab9.3/SongCollection.cs
@@ -7,61 +7,71 @@ using System.Threading.Tasks;
 
 namespace lab9._3
 {
-    public class SongCollection
+   public class SongCollection
+{
+    public Song[] Songs { get; set; } = new Song[0];
+
+    public void AddSong(Song song)
     {
-        public Song[] Songs { get; set; } = new();
-
-        public void AddSong(Song song) {
-            int newindex = Songs.Length;
-            Array.Resize(ref Songs, newindex + 1)
-                Songs[newindex] = song;
-        }
-
-        public bool RemoveSong(string title)
+      
+        Song[] temp = new Song[Songs.Length + 1];
+        
+        for (int i = 0; i < Songs.Length; i++)
         {
-            int index = Array.FindIndex(Songs, s => s.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
-            if (index != -1)
-            {
-                Songs = Songs.Where((_,i)=>i != index).Toarrat();
-                return true;
-            }
-            return false;
+            temp[i] = Songs[i];
         }
+        
+        temp[Songs.Length] = song;
+        Songs = temp;
+    }
 
-        public bool EditSong(string title, Song newSong)
+    public bool RemoveSong(string title)
+    {
+        int index = Array.FindIndex(Songs, s => s.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        if (index != -1)
         {
-            int index = Array.FindIndex(Songs, s => s.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
-            if (index != -1)
-            {
-                Songs[index] = newSong;
-                return true;
-            }
-            return false;
+            Songs = Songs.Where((_, i) => i != index).ToArray();
+            return true;
         }
+        return false;
+    }
 
-        public Song[] SearchByPerformer(string performer)
+    public bool EditSong(string title, Song newSong)
+    {
+        int index = Array.FindIndex(Songs, s => s.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        if (index != -1)
         {
-            return Songs.Where(s => s.Performers.Any(p => p.Equals(performer, StringComparison.OrdinalIgnoreCase))).ToArray();
+            Songs[index] = newSong;
+            return true;
         }
+        return false;
+    }
 
-        public Song[] Search(Func<Song, bool> predicate)
-        {
-            return Songs.Where(predicate).ToArray();
-        }
+    public Song[] SearchByPerformer(string performer)
+    {
+        return Songs.Where(s =>
+            s.Performers.Any(p => p.Equals(performer, StringComparison.OrdinalIgnoreCase))
+        ).ToArray();
+    }
 
-        public void SaveToFile(string filePath)
-        {
-            var json = JsonSerializer.Serialize(Songs, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(filePath, json);
-        }
+    public Song[] Search(Func<Song, bool> predicate)
+    {
+        return Songs.Where(predicate).ToArray();
+    }
 
-        public void LoadFromFile(string filePath)
+    public void SaveToFile(string filePath)
+    {
+        var json = JsonSerializer.Serialize(Songs, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, json);
+    }
+
+    public void LoadFromFile(string filePath)
+    {
+        if (File.Exists(filePath))
         {
-            if (File.Exists(filePath))
-            {
-                var json = File.ReadAllText(filePath);
-                Songs = JsonSerializer.Deserialize<Song[]>(json) ?? new Song[0];
-            }
+            var json = File.ReadAllText(filePath);
+            Songs = JsonSerializer.Deserialize<Song[]>(json) ?? new Song[0];
         }
     }
 }
+
